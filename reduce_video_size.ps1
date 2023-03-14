41f588374d35f14d6037673ae496f879247770d6
Kaky Lachapelle <lachapelle.kaky@gmail.com>
2023-03-14 23:50:32 +0300

    fix reduce video

--- reduce_video_size.ps1
+++ reduce_video_size.ps1
@@ -1,4 +1,4 @@
-$files = Get-ChildItem . -Recurse -Include  *.mp4,*.ps1
+$files = Get-ChildItem . -Recurse -Include  *.mp4, *.mov
 foreach ($f in $files){
     $outfile = $f.FullName + "_h265.mp4"
     $dt = Get-Date
@@ -6,4 +6,6 @@ foreach ($f in $files){
     ffmpeg -i $f.FullName -vcodec libx265 -crf 22 $outfile
     $dt = Get-Date
     Add-Content -Value "$dt Done $outfile" -Path ffmpeg_compression.log
+    Remove-Item -Path $f.FullName
+    Add-Content -Value "$dt Removed Done $f.FullName" -Path ffmpeg_compression.log
 }
